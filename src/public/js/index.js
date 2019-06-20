7bf275de5bde8ca91f6eb3c4dc3b2c58ccb5167a
Cristian Penteado <cristianpenteado@gmail.com>
2019-06-20 20:14:24 -0300

    add cards

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,30 +1,37 @@
 let box1 = document.getElementById('first-box');
 let box2 = document.getElementById('second-box');
 
-box1.addEventListener('change', changeValue);
-box2.addEventListener('change', changeValue);
-document.getElementById('battle').addEventListener('submit', (e) => {
+box1.addEventListener('change', showRadioField);
+box2.addEventListener('change', showRadioField);
+document.getElementById('battle').addEventListener('submit', async (e) => {
     e.preventDefault();
-    alert('You lose!!!')
-})
+    const radioValue = document.querySelector('input[name=field]:checked').value;
+    let value1;
+    let value2;
+    await fetch(`https://swapi.co/api/starships/?search=${box1.value}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(starships => {
+            value1 = starships.results[0][radioValue];
+            value1 = value1 == 'unknown' ? 0 : value1;
+        });
+    await fetch(`https://swapi.co/api/starships/?search=${box2.value}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(starships => {
+            value2 = starships.results[0][radioValue];
+            value2 = value2 == 'unknown' ? 0 : value2;
+        });
+    document.getElementById('result').style.display = await 'block';
 
-function changeValue() {
+});
+
+function showRadioField() {
     if (box2.value != -1 && box1.value != -1) {
-
-
-        fetch(`https://swapi.co/api/starships/?search=${box1.value}`)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(starships => {
-                document.getElementById('select-field').style.display = 'block';
-                //Pegando os valores
-                // let starship1 = document.createElement('div');
-                // starship1.innerHTML = 'Name: ' +  starships.results[0].name + '<br/>';
-                // starship1.innerHTML += 'Hyperdrive rating: ' + starships.results[0].hyperdrive_rating + '<br/>';
-                // starship1.innerHTML += 'Cargo capacity: ' + starships.results[0].cargo_capacity + '<br/>';
-                //document.body.appendChild(starship1);
-            })
-
+        document.getElementById('select-field').style.display = 'block';
+    } else {
+        document.getElementById('select-field').style.display = 'none';
     }
 }
